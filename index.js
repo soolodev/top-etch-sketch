08c4d8d9e93f334287b499b804b7f70ec4855377
soolo <soolo@soolo.dev>
2020-06-05 06:51:43 -0400

    Refactor to remove old code

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
 const BOX_COUNT = 8;
 
-const adjustBoxCount = (e) =>
-{
-    e.preventDefault();
-
-    let currCount = document.getElementById("box-count");
-    let scrollVal = parseInt(currCount.textContent);
-
-    if (e.deltaY > 0)
-    {
-        decrementBoxCount(scrollVal);
-
-        currCount.textContent = scrollVal - 1;
-    }
-    else if (e.deltaY < 0)
-    {
-        incrementBoxCount(scrollVal);
-
-        currCount.textContent = scrollVal + 1;
-    }
-}
-
-const changeBox = (e) =>
+const randomBackground = (e) =>
 {
     // from https://css-tricks.com/snippets/javascript/random-hex-color/
     e.target.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
@@ -30,72 +9,6 @@ const changeBox = (e) =>
 const initSite = () =>
 {
     let boxes = document.querySelectorAll(".box");
-    const boxContainer = document.querySelectorAll("#box-container");
 
-    boxes.forEach(box => box.addEventListener("mouseenter", e => changeBox(e)));
-
-    // for (let i = 0; i < BOX_COUNT; i++)
-    // {
-    //     for (let j = 0; j < BOX_COUNT; j++)
-    //     {
-            // let newBox = document.createElement("div");
-            // newBox.classList.add("box");
-            // newBox.addEventListener("mouseenter", e => changeBox(e));
-
-            // newBox.style.width = `${6}%`;
-            // newBox.style.width = `${(99.2 / BOX_COUNT) - 0.2}%`;
-            // newBox.style.height = `${5.3}%`;
-            // newBox.style.height = `${(88 / BOX_COUNT) - 0.2}%`;
-            // boxes.push(newBox);
-    //     }
-    // }
-
-    // for (let i = 0; i < BOX_COUNT; i++)
-    // {
-    //     for (let j = 0; j < BOX_COUNT; j++)
-    //     {
-    //         boxContainer.appendChild(boxes.pop());
-    //     }
-    // }
-}
-
-const changeBoxCount = (newCount) =>
-{
-    let boxes = document.querySelectorAll(".box");
-    const boxContainer = document.querySelector("#box-container");
-
-    boxes.forEach(box => box.remove());
-    boxes = [];
-
-    for (let i = 0; i < newCount; i++)
-    {
-        for (let j = 0; j < newCount; j++)
-        {
-            let newBox = document.createElement("div");
-            newBox.classList.add("box");
-            newBox.addEventListener("mouseenter", e => changeBox(e));
-
-            // newBox.style.width = `${99.2 / newCount}%`;
-            // newBox.style.height = `${88 / newCount}%`;
-            boxes.push(newBox);
-        }
-    }
-
-    for (let i = 0; i < newCount; i++)
-    {
-        for (let j = 0; j < newCount; j++)
-        {
-            boxContainer.appendChild(boxes.pop());
-        }
-    }
-}
-
-const incrementBoxCount = (currCount) =>
-{
-    changeBoxCount(currCount + 1);
-}
-
-const decrementBoxCount = (currCount) =>
-{
-    changeBoxCount(currCount - 1);
+    boxes.forEach(box => box.addEventListener("mouseenter", e => randomBackground(e)));
 }
