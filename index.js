9f1655bb30c3a32c8f5ee7e74b7ad747b0cbb250
soolo <soolo@soolo.dev>
2020-06-05 07:12:45 -0400

    Add adjustable box count and random color function to not choose white

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const BOX_COUNT = 8;
+const BOX_COUNT = 32;
+
+const randomColor = () =>
+{
+    // from https://css-tricks.com/snippets/javascript/random-hex-color/
+    let randColor = "FFFFFF";
+
+    // Do not accept white as a color
+    while (randColor == "FFFFFF")
+    {
+        randColor = Math.floor(Math.random()*16777215).toString(16);
+    }
+
+    return ("#" + randColor);
+}
 
 const randomBackground = (e) =>
 {
-    // from https://css-tricks.com/snippets/javascript/random-hex-color/
-    e.target.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    e.target.style.backgroundColor = randomColor();
 }
 
 const initPage = () =>
@@ -21,7 +34,7 @@ const initPage = () =>
         {
             let newBox = document.createElement("div");
             newBox.classList.add("box");
-            newBox.addEventListener("mouseenter", e =>  randomBackground(e));
+            newBox.addEventListener("mouseenter", e => randomBackground(e));
             newRow.appendChild(newBox);
         }
 
